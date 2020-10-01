7768483b61e823aaf62a1f1a4ceb3dee1db7dfff
lissella <69310460+lissella@users.noreply.github.com>
2020-10-01 11:43:34 +0700

    Add files via upload

---
+++ ProfileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+
+import ProfileScreen from './Screens/ProfileScreen '
+const ProfileScreen=()=>{
+    return(
+      <View style ={{flex:1,justifyContent:'center',alignItems:'center'}}>
+        <Text> ProfileScreen</Text>
+      </View>
+    )
+  }
+  export default ProfileScreen
